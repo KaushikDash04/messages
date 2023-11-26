936aa8d28dfcf638f6cc57271357e8281eb642b8
Kaushik Dash <63774072+KaushikDash04@users.noreply.github.com>
2023-11-26 18:23:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ yesBtn.addEventListener("click", () => {
   question.innerHTML = "Haan je 🙈🙈";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
-  // yes-btn.style.display='none';
+  yesBtn.style.display='none';
 });
 
 noBtn.addEventListener("click", () => {
