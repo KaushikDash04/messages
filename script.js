c9af773bc973c9e14b5cbb9be729dba36ae4c604
Kaushik Dash <63774072+KaushikDash04@users.noreply.github.com>
2023-11-26 18:12:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ noBtn.addEventListener("click", () => {
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
 
-noBtn.addEventListener("mouseover", () => {
-  const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - noBtnRect.width;
-  const maxY = window.innerHeight - noBtnRect.height;
+// noBtn.addEventListener("mouseover", () => {
+//   const noBtnRect = noBtn.getBoundingClientRect();
+//   const maxX = window.innerWidth - noBtnRect.width;
+//   const maxY = window.innerHeight - noBtnRect.height;
 
-  const randomX = Math.floor(Math.random() * maxX);
-  const randomY = Math.floor(Math.random() * maxY);
+//   const randomX = Math.floor(Math.random() * maxX);
+//   const randomY = Math.floor(Math.random() * maxY);
 
-  noBtn.style.left = randomX + "px";
-  noBtn.style.top = randomY + "px";
-});
+//   noBtn.style.left = randomX + "px";
+//   noBtn.style.top = randomY + "px";
+// });
